e605acf45cb03d28c363bf7d805a269a6449bc65
sahil malekar <sahilmalekar23@gmail.com>
2024-03-12 08:02:26 +0000

    chnages in hero img

--- qtify/src/Components/Hero/Hero.jsx
+++ qtify/src/Components/Hero/Hero.jsx
@@ -3,17 +3,18 @@ import styles from "./Hero.module.css";
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div>
-        <h1>100 Thousand Songs , ad-free</h1>
+      <div className={styles.heroText}>
+        <h1>100 Thousand Songs, ad-free</h1>
         <h1>Over thousands podcast episodes</h1>
       </div>
-      {/* <div>
+      <div className={styles.heroImg}>
         <img
           src={require("../../assets/hero_headphones.png")}
-          width={112}
+          width={212}
+          height="100%"
           alt="headphones"
         />
-      </div> */}
+      </div>
     </div>
   );
 };
