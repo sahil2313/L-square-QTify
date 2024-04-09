14efa2bd69c2e376445c782e2c72c6593b30fd5f
sahil2313 <sahilmalekar23@gmail.com>
2024-04-09 13:30:09 +0530

    Search functionality done

--- qtify/src/Components/Search/Search.jsx
+++ qtify/src/Components/Search/Search.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Search.module.css";
 import { ReactComponent as SearchIcon } from "../../assets/search-icon.svg";
-function Search({ placeholder }) {
+
+function Search({ placeholder, searchData }) {
+  const [key, setKey] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    if (!key.trim()) {
+      setFilteredData([]);
+      return;
+    }
+
+    const data = searchData.filter((album) =>
+      album.title.toLowerCase().includes(key.toLowerCase()),
+    );
+    setFilteredData(data);
+
+    return () => {
+      setFilteredData([]);
+    };
+  }, [key, searchData]);
+
   const onSubmit = (e) => {
     e.preventDefault();
   };
+
   return (
     <>
       <form className={styles.wrapper} onSubmit={onSubmit}>
         <input
           className={styles.search}
           placeholder={placeholder}
+          value={key}
+          onChange={(e) => setKey(e.target.value)}
           type="text"
           required
         />
@@ -18,6 +41,24 @@ function Search({ placeholder }) {
           <SearchIcon />
         </button>
       </form>
+      {filteredData.length > 0 && (
+        <div className={styles.searchListContainer}>
+          {filteredData.map((album) => (
+            <div key={album.id} className={styles.searchListItem}>
+              <div className={styles.listContent}>
+                <img src={album.image} alt={album.title} />
+                <div className={styles.listTitle}>
+                  <p className={styles.albumTitle}>{album.title}</p>
+                  <p style={{ fontSize: "12px" }}>
+                    {album.description.slice(0, 30)}
+                  </p>
+                </div>
+              </div>
+              <p style={{ fontSize: "12px" }}>{`${album.follows} Follows`}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 }
